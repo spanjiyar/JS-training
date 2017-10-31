a165c533e87d9e78a26927ccfe3d96885e60abc7
spanjiyar <ppsanju97@gmail.com>
2017-10-31 12:48:50 +0530

    Assignment1

--- Assignment1/src/node.js
+++
@@ -1,9 +0,0 @@
-class Node {
-    constructor(data = null, prev = null, next = null) {
-        this.data = data;
-        this.prev = prev;
-        this.next = next;
-    }
-}
-
-module.exports = Node;
